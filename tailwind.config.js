6030ca63e5e8453517339563b73ec095fcb8021d
tercoder <tuyishimirebt12@gmail.com>
2023-07-30 01:05:55 +0200

    adding initial-configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.{html,js}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    colors: {
+      bg: {
+        dm: "#141D2F",
+        lm: "#F6F8FF",
+      },
+      "bg-content": {
+        dm: "#1E2A47",
+        lm: "#FEFEFE",
+      },
+      text: {
+        dm: "#ffffff",
+        lm: "#4B6A9B",
+      },
+      "text-alt": {
+        dm: "#ffffff",
+        lm: "#2B3442",
+      },
+      "dm-shadow": {
+        dark: "rgba(70,88,109,0.15)",
+        light: "rgba(70, 88, 109, 0.25)",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: "Space Mono",
+        mono: "monospace",
+      },
+    },
   },
   plugins: [],
 };
